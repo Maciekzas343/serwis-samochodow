f9c08e11779890165b0b3d88de1845d14029e8a4
Maciekzas343 <maciekzas@outlook.com>
2024-10-29 12:06:01 +0100

    Save

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client";
 
-const element = <div></div>;
+const currentDate = new Date();
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+
+function getRandomNumber() {
+  return Math.round(Math.random() * 100);
+}
+
+const number = getRandomNumber();
+
+const element = <div>{number}</div>;
+console.log(hours, minutes);
 
 createRoot(document.getElementById("root")).render(element);
