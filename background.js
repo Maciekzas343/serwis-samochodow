c87d5da82e7cece900785f2fbc58198ef0a230f9
Maciekzas343 <maciekzas@outlook.com>
2024-11-05 13:39:13 +0100

    Update background.js

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 let currentBackground = 0; // Indeks aktualnego tła
 const backgrounds = [
-  'url("/background.jpg")',
-  'url("/background1.jpg")',
-  'url("/background2.jpg")',
+  'url("background.jpg")',
+  'url("background1.jpg")',
+  'url("background2.jpg")',
 ];
 
 function changeBackground() {
