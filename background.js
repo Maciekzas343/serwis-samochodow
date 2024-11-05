7fba1a90b81e2be21cec7ceeb80ad320b64945b0
Maciekzas343 <maciekzas@outlook.com>
2024-11-05 12:59:56 +0100

    update 1.02

--- background.js
+++ background.js
@@ -1,20 +1,18 @@
 let currentBackground = 0; // Indeks aktualnego tła
 const backgrounds = [
-    'url("background.jpg")',
-    'url("background1.jpg")',
-	'url("background2.jpg")'
+  'url("public/background.jpg")',
+  'url("public/background1.jpg")',
+  'url("public/background2.jpg")',
 ];
 
 function changeBackground() {
-    // Zmiana tła
-    document.body.style.backgroundImage = backgrounds[currentBackground];
-    
-    // Przełącz na następne tło
-    currentBackground = (currentBackground + 1) % backgrounds.length; 
+  // Zmiana tła
+  document.body.style.backgroundImage = backgrounds[currentBackground];
+
+  // Przełącz na następne tło
+  currentBackground = (currentBackground + 1) % backgrounds.length;
 }
 
-
 setInterval(changeBackground, 15000);
-
 
 changeBackground();
